4566a0932e1ec19249971f3b989f3aaa02bc69e3
2025246 <rmansfield@creightonprep.org>
2024-11-01 07:58:15 -0500

    lift pids done and now auto finally time

--- TeamCode/src/main/java/indubitables/config/util/RobotConstants.java
+++ TeamCode/src/main/java/indubitables/config/util/RobotConstants.java
@@ -29,7 +29,7 @@ public class RobotConstants {
 
     public static int liftToZero = 100;
     public static int liftToHumanPlayer = 100;
-    public static int liftToHighChamber = 315;
+    public static int liftToHighChamber = 200;
     public static int liftToHighBucket = 0;
     public static int liftToTransfer = 200;
     public static int liftToPark = 0;
