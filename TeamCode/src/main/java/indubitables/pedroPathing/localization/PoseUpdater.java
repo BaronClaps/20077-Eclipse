4eebf9d42d49cf9ea95f25b787cad096af15b7f6
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2024-12-22 15:48:30 -0600

    update gradle and stuff

--- TeamCode/src/main/java/indubitables/pedroPathing/localization/PoseUpdater.java
+++ TeamCode/src/main/java/indubitables/pedroPathing/localization/PoseUpdater.java
@@ -344,7 +344,7 @@ public class PoseUpdater {
     /**
      *
      */
-    public void resetIMU() {
+    public void resetIMU() throws InterruptedException {
         localizer.resetIMU();
     }
 }
